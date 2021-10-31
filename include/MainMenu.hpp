0fb04224fc99d77c17fe3054962c76f1b3cee3b3
hatemets <mistermark009@gmail.com>
2021-10-31 22:46:27 +0200

    Filter source code from unused files, improve World

--- include/MainMenu.hpp
+++ include/MainMenu.hpp
@@ -1,1 +1,21 @@
+#ifndef MAIN_MENU_HPP
+#define MAIN_MENU_HPP
 
+#include <SFML/Graphics.hpp>
+#include "Node.hpp"
+#include "ResourceHolder.hpp"
+#include "auxiliary/ResourceIdentifiers.hpp"
+
+class MainMenu
+{
+	public:
+		MainMenu(sf::RenderWindow& window);
+		void update(sf::Time deltaTime);
+		void draw();
+
+	private:
+		sf::RenderWindow& window_;
+};
+
+
+#endif
--- include/World.hpp
+++ include/World.hpp
@@ -1,1 +1,39 @@
+#ifndef WORLD_HPP
+#define WORLD_HPP
 
+#include <SFML/Graphics.hpp>
+#include <SFML/System/NonCopyable.hpp>
+#include "Node.hpp"
+#include "ResourceHolder.hpp"
+#include "ResourceIdentifiers.hpp"
+
+class World
+{
+	public:
+		explicit World(sf::RenderWindow& window);
+		void operate(sf::Time deltaTime);
+
+	public:
+		enum class States
+		{
+			Menu,
+			Pause,
+			Level
+		};
+	
+	private:
+		enum class Layers
+		{
+			Background,
+			Entities,
+			HUD,
+			// total count to get the number of members in the enum
+			totalCount	 
+		};
+
+	private:
+		sf::RenderWindow& window_;
+		States currentState_;
+};
+
+#endif
