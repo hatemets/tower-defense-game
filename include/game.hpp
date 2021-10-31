efe331b867f707f7591a5efe7bd9dc00e292e647
hatemets <mistermark009@gmail.com>
2021-10-31 22:35:47 +0200

    Implement main.cpp and Game

--- include/game.hpp
+++ include/game.hpp
@@ -1,1 +1,30 @@
+#ifndef GAME_HPP
+#define GAME_HPP
 
+#include <SFML/Graphics.hpp>
+#include "constants.hpp"
+
+class Game
+{
+	public:
+		Game();
+		void run();
+
+		// Frames per second is equal to the inverese of TimePerFrame
+		sf::Time TimePerFrame = sf::seconds(1.f / FramesPerSecond);
+
+	private:
+		void processEvents();
+		void update(sf::Time deltaTime);
+		void render();
+		void handleUserInput();
+
+	private:
+		sf::RenderWindow window_;
+		// TODO:
+		// Main menu
+		// (Level Menu)
+		// Levels
+};
+
+#endif
--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,67 @@
+#include "../include/game.hpp"
+#include <SFML/System/Time.hpp>
 
+Game::Game()
+	: window_{sf::VideoMode(WindowWidth, WindowHeight), "Tower Defense Game"}
+{
+}
+
+void Game::run()
+{
+	// Counts time between frames
+	sf::Clock clock;
+
+	// Initialize time to zero
+	sf::Time elapsedTime = sf::Time::Zero;
+
+	while (window_.isOpen())
+	{
+		elapsedTime += clock.restart();
+
+		// If elapsed time is larger, cut it down by the amount of time passed
+		// so far (achieves a fixed timestep)
+		while (elapsedTime > TimePerFrame)
+		{
+			elapsedTime -= clock.restart();
+			processEvents();
+			update(TimePerFrame);
+		}
+
+		render();
+	}
+}
+
+void Game::processEvents()
+{
+	sf::Event event;
+
+	// Listen for events
+	// TODO: add an event listener for mouseclicks
+	while (window_.pollEvent(event))
+	{
+		switch (event.type)
+		{
+			case sf::Event::Closed:
+				window_.close();
+				break;
+		}
+	}
+}
+
+// Update game with fixed timesteps
+// Otherwise, a fast computer would render the game more often and thus have
+// the entities move faster
+void Game::update(sf::Time deltaTime)
+{
+}
+
+void Game::render()
+{
+	window_.clear();
+
+	window_.display();
+}
+
+void Game::handleUserInput()
+{
+}
