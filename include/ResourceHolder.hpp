17588f4aca9102e73d90e3abf86f631ac5bfdc73
hatemets <mistermark009@gmail.com>
2021-10-31 22:26:15 +0200

    Add basic resource management

--- include/ResourceHolder.hpp
+++ include/ResourceHolder.hpp
@@ -1,1 +1,26 @@
+#ifndef RESOURCE_HOLDER_HPP
+#define RESOURCE_HOLDER_HPP
 
+#include <SFML/Graphics.hpp>
+#include <map>
+#include <memory>
+#include <cassert>
+#include "auxiliary/Button.hpp"
+
+template <typename Resource, typename Identifier>
+class ResourceHolder
+{
+	public:
+		void load(Identifier id, const std::string& filename);
+		void load(sf::Vector2f size, sf::Color color, const std::string& text);
+
+		Resource& get(Identifier id);
+		const Resource& get(Identifier id) const;
+
+	private:
+		std::map<Identifier, std::unique_ptr<Resource>> resources_;
+};
+
+#include "ResourceHolder.inl"
+
+#endif
