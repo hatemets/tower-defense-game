ef5c3a69ca7bfd6cf7d8aabd948c2ec20b0a5755
hatemets <mistermark009@gmail.com>
2021-11-17 21:47:08 +0200

    Set the aspect ratio to 4:3

--- include/auxiliary/constants.hpp
+++ include/auxiliary/constants.hpp
@@ -3,16 +3,21 @@
 
 // General
 const unsigned int FramesPerSecond = 200;
-const int TileSize = 40;
-const int TileRows = 20;
-const int TileCols = 20;
-const int WindowWidth = TileSize * TileCols;
-const int WindowHeight = TileSize * TileRows;
+
+// NOTE: The window MUST have a resolution of 4:3
+// TODO: Write a guard for ensuring the aspect ratio
+const int WindowWidth = 800;
+const int WindowHeight = 600;
+
+const int TileCols = 40;
+const int TileRows = 30;
+
+const int TileSize = WindowWidth / TileCols;
 
 // Math
-const float Pi = 3.14159;
-const float RadiansToDegrees = 180 / Pi;
-const float DegreesToRadians = Pi / 180;
+const float Pi = 3.14159f;
+const float RadiansToDegrees = 180.f / Pi;
+const float DegreesToRadians = Pi / 180.f;
 
 // Buttons
 const float ButtonWidth = 1.f / 4.f * WindowWidth;
