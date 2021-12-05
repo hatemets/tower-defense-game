9337b4a967c65dbf4835192b2896a081349b3577
hatemets <heidmetsmark@gmail.com>
2021-12-05 22:59:16 +0200

    Add exit button without functionality

--- include/auxiliary/constants.hpp
+++ include/auxiliary/constants.hpp
@@ -16,6 +16,9 @@ const int TileRows = 15;
 
 const int TileSize = WindowWidth / TileCols;
 
+// The space between buttons
+const float ButtonMargin = 20.f;
+
 // Primary theme color for buttons
 const std::array<int, 3> PrimaryColor = {00, 00, 80};
 
@@ -31,8 +34,8 @@ const float DegreesToRadians = Pi / 180.f;
 const float ButtonWidth = 1.f / 4.f * WindowWidth;
 const float ButtonHeight = 1.f / 6.f * WindowHeight;
 
-const float ButtonPaddingX = 1.f / 4.f * ButtonWidth;
-const float ButtonPaddingY = 1.f / 8.f * ButtonHeight;
+const float ButtonPaddingX = 0.3f * ButtonWidth;
+const float ButtonPaddingY = 0.15f * ButtonHeight;
 
 // Credits
 const int NewGameCredits = 300;
--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -16,7 +16,9 @@ MainMenu::MainMenu(sf::RenderWindow& window)
 void MainMenu::loadResources()
 {
 	textures_.load(Textures::ID::GrassArea, "./include/images/background1.jpg");
+
 	buttonShapes_.load(Buttons::ID::LevelMenuButton);
+	buttonShapes_.load(Buttons::ID::QuitButton);
 }
 
 
@@ -45,11 +47,15 @@ void MainMenu::addButtons()
 {
 	// Start button
 	auto startButton = std::make_unique<Button>("Play", fonts_, Fonts::ID::SourceCodePro, buttonShapes_, Buttons::ID::LevelMenuButton);
-	startButton->setPosition(WindowWidth / 2.f, WindowHeight / 2.f);
+    sf::Vector2f startBtnPos(WindowWidth / 2.f, WindowHeight / 2.f);
+	startButton->setPosition(startBtnPos.x, startBtnPos.y);
 	buttons_.push_back(startButton.get());
 	layers_[static_cast<std::size_t>(Layers::Buttons)]->addChild(std::move(startButton));
 
-	// TODO: Add quit button
+	auto quitButton = std::make_unique<Button>("Exit", fonts_, Fonts::ID::SourceCodePro, buttonShapes_, Buttons::ID::QuitButton);
+	quitButton->setPosition(startBtnPos.x, startBtnPos.y + quitButton->getButton().getSize().y + ButtonMargin);
+	buttons_.push_back(quitButton.get());
+	layers_[static_cast<std::size_t>(Layers::Buttons)]->addChild(std::move(quitButton));
 }
 
 
