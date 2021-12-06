dee9e8a692ee621616141f5caa6d6d51a9d145ba
hatemets <heidmetsmark@gmail.com>
2021-12-06 14:36:27 +0200

    Improve popup stylnig

--- src/Message.cpp
+++ src/Message.cpp
@@ -3,13 +3,13 @@
 
 Message::Message(const std::string& msg)
     : message_(msg),
-    messageBox_(sf::Vector2f(400.f, 200.f)),
+    messageBox_(sf::Vector2f(400.f, 100.f)),
     isActive_(false)
 {
     auto center = sf::Vector2f(WindowWidth / 2.f, WindowHeight / 2.f);
 
     messageBox_.setOrigin(messageBox_.getSize().x / 2.f, messageBox_.getSize().y / 2.f);
-    messageBox_.setPosition(center.x, center.y + 50.f);
+    messageBox_.setPosition(center.x, center.y);
     messageBox_.setFillColor(sf::Color(54, 87, 176));
 
     font_.loadFromFile("./include/fonts/SourceCodePro.ttf");
@@ -20,7 +20,7 @@ Message::Message(const std::string& msg)
 
     auto bounds = text_.getGlobalBounds();
     text_.setOrigin((bounds.width + bounds.left) / 2.f, (bounds.height + bounds.top) / 2.f);
-    text_.setPosition(center);
+    text_.setPosition(center.x, center.y - 5.f);
 }
 
 void Message::drawSelf(sf::RenderTarget& target, sf::RenderStates states) const
